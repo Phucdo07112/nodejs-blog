c9fadbd68442ae090bc86ccdee40ff6792972a01
Phucdo07112 <85002157+Phucdo07112@users.noreply.github.com>
2022-04-25 16:32:53 +0700

    ok
    ok

--- blog/src/index.js
+++ blog/src/index.js
@@ -3,13 +3,23 @@ const path = require('path')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 
+const route = require('./routes')
+
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+  extended: true
+})) // đây là middleware để sử lý dữ liệu từ form submit lên cho chúng ta, urlencoded sử lý dạng form
+app.use(express.json()) // dạng gửi từ code js lên thì có ex.json sử lý
+
+// XMLHttpRequest, fetch, axios, gửi phương thức post get đều được và có thể gửi lên phía server cho ta chứ ko phải gửi dạng form
+// use sử dụng
+
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // Template engine
 app.engine('hbs', handlebars.engine({
@@ -18,14 +28,16 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// Routes init: khởi tạo tuyến đường
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// { Query parameters là khái niệm để giúp mình có thể truyền được dữ liệu qua chính URL của mình
+// nó tồn tại trong tất cả các METHOD sử dụng = ?value=...&value&...
+// thường được sử dụng với GET method }
+
+// GET method nhận dữ liệu từ phía server, từ phía client
+// POST method phi muốn gửi dữ liệu từ phía client lên trên phía server (người dùng lên máy chủ)
---
+++ blog/src/routes/index.js
@@ -0,0 +1,20 @@
+const newsRouter = require('./news')
+const siteRouter = require('./site')
+
+function route(app) {
+
+      // request nó chứa thông tin yêu cầu gửi lên
+      // response nó chứa lựa chọn tùy chỉnh qua response , phản hồi ngược lại cho client
+
+    app.use('/news', newsRouter)
+
+    app.use('/', siteRouter)
+      
+      
+    // app.post('/search', (req, res) => {
+    //     console.log(req.body); // qs / body-parser thư viện có sẳn
+    //     res.send('');
+    // })
+}
+
+module.exports = route;
